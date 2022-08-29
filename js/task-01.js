45b9ebcbf1d3153940073d87ec5d8d2d6e6eed39
SuhakMV <sugamish@gmail.com>
2022-08-29 14:28:09 +0300

    29081428

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,7 @@
-const numberOfCategories = document.querySelectorAll("li.item");
-console.log(numberOfCategories.length);
+const catEl = document.querySelectorAll("li.item");
+console.log('Number of categories:', catEl.length);
+
+catEl.forEach(function (categories){
+    console.log("Category:",categories.firstElementChild.textContent);
+    console.log("Elements:",categories.lastElementChild.children.length);
+} )
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('ul');
+//console.log(ulEl);
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+const ing = ingredients[i];
+const liEl = document.createElement('li');
+  liEl.textContent = ing;
+  liEl.classList.add('item');
+
+  elements.push(liEl);}
+ulEl.append(...elements);
+
