8c641c0de1b3dc15f1b40dd18dd3d48ab99b7d24
SuhakMV <sugamish@gmail.com>
2022-08-31 19:56:51 +0300

    31081956

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  span.textContent = getRandomHexColor();
+  document.body.setAttribute(
+    'style',
+    'background-color:' + span.textContent
+  );});
